fb34c6cc32106d854a8b621cbdee875acee4307a
Faith Alvarado <faith.alvarado@udea.edu.co>
2024-11-19 14:09:17 -0500

    Update nivel.h

--- nivel.h
+++ nivel.h
@@ -1,18 +1,28 @@
 #ifndef NIVEL_H
 #define NIVEL_H
 
-#include <QGraphicsScene>
-#include <QObject>
 #include "jugador.h"
 #include "objetos.h"
+
+#include <QGraphicsScene>
+#include <QObject>
+#include <QGraphicsPixmapItem>
+#include <QTimer>
+#include <QDebug>
+
 
 class Nivel : public QObject{
     Q_OBJECT
 private:
     short int nivelSeleccionado;
+    QGraphicsScene* escena;
+    QGraphicsPixmapItem* edificioItem;
+    int yOffset;
 
 public:
     Nivel(short int nivelSeleccionado, QGraphicsScene * escena);
+    void moverEdificio();
+    void actualizarCamara();
     Jugador * jugador;
     Objetos* objeto;
     virtual ~Nivel();
