36ad3a1e8b48f49c9a5963babc2b51159e3d9129
CarlaZapata14 <carla.zapata@udea.edu.co>
2024-12-01 05:45:26 -0500

    Update bart.cpp

--- bart.cpp
+++ bart.cpp
@@ -184,30 +184,14 @@ void Bart::actualizarAnimacion(){
 
 
 void Bart::keyReleaseEvent(QKeyEvent *event) {
-    switch(event->key()){
-    case Qt::Key_A:
-    case Qt::Key_D:
-    case Qt::Key_W:
-    case Qt::Key_S:
-    case Qt::Key_Space:
-        keys[event->key()]=false;
-        break;
-    default:
-        break;
+    if (keys.contains(event->key())) {
+        keys[event->key()] = false;
     }
 }
 
 void Bart::keyPressEvent(QKeyEvent *event){
-    switch(event->key()){
-    case Qt::Key_A:
-    case Qt::Key_D:
-    case Qt::Key_W:
-    case Qt::Key_S:
-    case Qt::Key_Space:
-        keys[event->key()]=true;
-        break;
-    default:
-        break;
+    if (keys.contains(event->key())) {
+        keys[event->key()] = true;
     }
 
     if (event -> key() == Qt::Key_Space && disparar){
